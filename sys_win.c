b5a9a9608f14cd3ddcedbbb3ea81d9781ac23f1d
Philip Buuck <philipbuuck@gmail.com>
2016-01-11 19:22:23 -0500

    Handmade Quake 1.3
    Replacing strcmp() and atoi()

--- sys_win.c
+++ sys_win.c
@@ -1,26 +1,89 @@
+#include <stdint.h>
 #include <windows.h>
 
-#define MAX_NUM_ARGVS 50
+typedef uint8_t uint8;
+typedef int32_t int32;
+typedef uint32_t uint32;
 
-int argc = 1;
-char *largv[MAX_NUM_ARGVS];
+#define MAX_NUM_ARGVS 3
 
-int COM_CheckParm(char *parm)
+int32 argc = 1;
+uint8 *largv[MAX_NUM_ARGVS+1];
+
+int32 Q_strcmp(uint8 *s1, uint8 *s2)
 {
-    for (int i = 1; i < argc; i++)
+    while (*s1 == *s2)
     {
-        if (!strcmp(parm, largv[i]))
+        if (!*s1)
+            return 0;
+        ++s1;
+        ++s2;
+    }
+
+    return ((*s1 < *s2) ? -1 : 1);
+}
+
+// decimal or hexadecimal
+// negative and positive
+// "50" "12" "0x4321464fd" "0X4AbdC"
+int32 Q_atoi(uint8 *str)
+{
+    int32 sign = 1;
+    int32 val = 0;
+    uint8 c;
+
+    if (*str == '-')
+    {
+        sign = -1;
+        str++;
+    }
+
+    // hexadecimal
+    if (str[0] == '0' && (str[1] == 'x' || str[1] == 'X'))
+    {
+        str += 2;
+        while (1)
+        {
+            c = *str;
+            str++;
+            if (c >= '0' && c <= '9')
+                val = val * 16 + c - '0';
+            else if (c >= 'a' && c <= 'f')
+                val = val * 16 + c - 'a' + 10;
+            else if (c >= 'A' && c <= 'F')
+                val = val * 16 + c - 'A' + 10;
+            else
+                return val*sign;
+        }
+    }
+
+    // decimal
+    while (1)
+    {
+        c = *str;
+        str++;
+        if (c < '0' || c > '9')
+            return sign * val;
+        val = val * 10 + c - '0';
+    }
+}
+
+int32 COM_CheckParm(uint8 *parm)
+{
+    for (int32 i = 1; i < argc; i++)
+    {
+        if (!Q_strcmp(parm, largv[i]))
             return i;
     }
 
     return 0;
 }
 
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+int32 CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int32 nCmdShow)
 {
     largv[0] = "";
 
-    while (*lpCmdLine && (argc < MAX_NUM_ARGVS))
+    while (*lpCmdLine && (argc < MAX_NUM_ARGVS+1))
     {
         while (*lpCmdLine && ((*lpCmdLine <= 32) || (*lpCmdLine > 126)))
             lpCmdLine++;
@@ -41,9 +104,7 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
         }
     }
 
-    largv[argc] = "";
-
-    int test = COM_CheckParm("-setalpha");
-    int value = (int)(*largv[3]);
+    int32 test = COM_CheckParm("-setalpha");
+    int32 value = Q_atoi(largv[test+1]);
     return 0;
 }
