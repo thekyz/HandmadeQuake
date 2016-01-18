0a9385e76b9e6700f93f5b96e42c40fbb4f00ce3
Philip Buuck <philipbuuck@gmail.com>
2016-01-18 08:13:12 -0500

    Handmade Quake 1.5
    const, and the Module 1 wrapup

--- q_stl.c
+++ q_stl.c
@@ -1,6 +1,6 @@
 #include "quakedef.h"
 
-void Q_strcpy(uint8 *dest, uint8 *src)
+void Q_strcpy(char *dest, const char *src)
 {
     while (*src)
     {
@@ -9,7 +9,7 @@ void Q_strcpy(uint8 *dest, uint8 *src)
     *dest = 0;
 }
 
-void Q_strncpy(uint8 *dest, uint8 *src, int32 count)
+void Q_strncpy(char *dest, const char *src, int32 count)
 {
     if (count < 0)
         return;
@@ -27,7 +27,7 @@ void Q_strncpy(uint8 *dest, uint8 *src, int32 count)
     }
 }
 
-int32 Q_strlen(uint8 *str)
+int32 Q_strlen(const char *str)
 {
     int32 count = 0;
     while (str[count])
@@ -36,7 +36,7 @@ int32 Q_strlen(uint8 *str)
     return count;
 }
 
-int32 Q_strcmp(uint8 *s1, uint8 *s2)
+int32 Q_strcmp(const char *s1, const char *s2)
 {
     while (*s1 == *s2)
     {
@@ -51,7 +51,7 @@ int32 Q_strcmp(uint8 *s1, uint8 *s2)
 
 // decimal or hexadecimal
 // negative and positive
-int32 Q_atoi(uint8 *str)
+int32 Q_atoi(const char *str)
 {
     int32 sign = 1;
     int32 val = 0;
