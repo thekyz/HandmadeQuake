b323c894cb4f6d38d8167b72dc5c13e28072e09e
thekyz <thekyz@gmail.com>
2016-01-21 17:27:09 +0100

    Re-ordered code a bit to match with module 2.2

--- main.c
+++ main.c
@@ -8,6 +8,89 @@
 #include <string.h>
  
 #include <sys/utsname.h>
+
+bool IsRunning = true;
+
+static void handleEvent(Display *dpy, int s, Window mainwindow, Atom atom, bool uname_ok, struct utsname sname, XEvent *e)
+{
+  switch (e->type)
+  {
+    case Expose:
+      {
+        int y_offset = 20;
+
+        const char* s1 = "X11 test app under Linux";
+        const char* s2 = "by thekyz";
+
+        XSetForeground(dpy, DefaultGC(dpy, s), 0xffffff);
+
+        XDrawString(dpy, mainwindow, DefaultGC(dpy, s), 10, y_offset, s1, strlen(s1));
+        y_offset += 20;
+        XDrawString(dpy, mainwindow, DefaultGC(dpy, s), 10, y_offset, s2, strlen(s2));
+        y_offset += 20;
+
+        if (uname_ok)
+        {
+          char buf[256] = {0};
+          
+          sprintf(buf, "System information:");
+          XDrawString(dpy, mainwindow, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
+          y_offset += 15;
+
+          sprintf(buf, "- System: %s", sname.sysname);
+          XDrawString(dpy, mainwindow, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
+          y_offset += 15;
+
+          sprintf(buf, "- Release: %s", sname.release);
+          XDrawString(dpy, mainwindow, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
+          y_offset += 15;
+
+          sprintf(buf, "- Version: %s", sname.version);
+          XDrawString(dpy, mainwindow, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
+          y_offset += 15;
+          
+          sprintf(buf, "- Machine: %s", sname.machine);
+          XDrawString(dpy, mainwindow, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
+          y_offset += 20;
+        }
+
+
+        XWindowAttributes  wa;
+        XGetWindowAttributes(dpy, mainwindow, &wa);
+        int width = wa.width;
+        int height = wa.height;
+        char buf[128]={0};
+        sprintf(buf, "Current window size: %dx%d", width, height);
+        XDrawString(dpy, mainwindow, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
+        y_offset += 20;
+      }
+      break;
+
+    case KeyPress:
+      {
+        char buf[128] = {0};
+        KeySym keysym;
+        int len = XLookupString(&e->xkey, buf, sizeof buf, &keysym, NULL);
+        if (keysym == XK_Escape)
+        {
+          IsRunning = false;
+          break;
+        }
+      }
+      break;
+
+    case ClientMessage:
+      if ((int)(e->xclient.data.l[0]) == atom)
+      {
+        IsRunning = false;
+      }
+      break;
+
+    default:
+      // Do nothing ...
+      break;
+  }
+}
  
 int main(int argc, char** argv)
 {
@@ -24,7 +107,7 @@ int main(int argc, char** argv)
   XMapWindow(dpy, mainwindow);
  
   XStoreName(dpy, mainwindow, "X11 window");
-  
+
   Atom WM_DELETE_WINDOW = XInternAtom(dpy, "WM_DELETE_WINDOW", False); 
   XSetWMProtocols(dpy, mainwindow, &WM_DELETE_WINDOW, 1);  
  
@@ -37,71 +120,12 @@ int main(int argc, char** argv)
   }
  
   XEvent e;
-  while (1) 
+  while (IsRunning) 
   {
-    XNextEvent(dpy, &e);
-    if (e.type == Expose) 
+    // XNextEvent(dpy, &e);
+    if (XCheckMaskEvent(dpy, ~NoEventMask, &e))
     {
-      int y_offset = 20;
- 
-      const char* s1 = "X11 test app under Linux";
-      const char* s2 = "by thekyz";
-
-      XSetForeground(dpy, DefaultGC(dpy, s), 0xffffff);
-
-      XDrawString(dpy, mainwindow, DefaultGC(dpy, s), 10, y_offset, s1, strlen(s1));
-      y_offset += 20;
-      XDrawString(dpy, mainwindow, DefaultGC(dpy, s), 10, y_offset, s2, strlen(s2));
-      y_offset += 20;
- 
-      if (uname_ok)
-      {
-        char buf[256] = {0};
-        
-        sprintf(buf, "System information:");
-        XDrawString(dpy, mainwindow, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
-        y_offset += 15;
- 
-        sprintf(buf, "- System: %s", sname.sysname);
-        XDrawString(dpy, mainwindow, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
-        y_offset += 15;
- 
-        sprintf(buf, "- Release: %s", sname.release);
-        XDrawString(dpy, mainwindow, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
-        y_offset += 15;
- 
-        sprintf(buf, "- Version: %s", sname.version);
-        XDrawString(dpy, mainwindow, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
-        y_offset += 15;
-        
-        sprintf(buf, "- Machine: %s", sname.machine);
-        XDrawString(dpy, mainwindow, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
-        y_offset += 20;
-      }
- 
- 
-      XWindowAttributes  wa;
-      XGetWindowAttributes(dpy, mainwindow, &wa);
-      int width = wa.width;
-      int height = wa.height;
-      char buf[128]={0};
-      sprintf(buf, "Current window size: %dx%d", width, height);
-      XDrawString(dpy, mainwindow, DefaultGC(dpy, s), 10, y_offset, buf, strlen(buf));
-      y_offset += 20;
-    }
-    
-    if (e.type == KeyPress)
-    {
-      char buf[128] = {0};
-      KeySym keysym;
-      int len = XLookupString(&e.xkey, buf, sizeof buf, &keysym, NULL);
-      if (keysym == XK_Escape)
-        break;
-    }
- 
-    if ((e.type == ClientMessage) && ((int)(e.xclient.data.l[0]) == WM_DELETE_WINDOW))
-    {
-      break;
+      handleEvent(dpy, s, mainwindow, WM_DELETE_WINDOW, uname_ok, sname, &e);
     }
   }
   
